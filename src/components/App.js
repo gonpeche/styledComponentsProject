56c0128682eed6d23ec6c6edb907566cfe9e8f04
gonpeche <espeche.g@gmail.com>
2020-09-20 13:49:51 -0300

    prettier config

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'components/common/Button'
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button>Test</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/common/Button.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    color: white;
+    background: #f8049c;
+    font-weight: bold;
+    padding: 8px;
+    border-radius: 4px;
+    box-shadow: none;
+    font-size: 1em;
+    border: none;
+    width: 100%;
+    display: block;
+    white-space: none;
+
+    &:disabled {
+        background: #eee;
+        color: #666;
+    }
+`;
+
+export default Button;
