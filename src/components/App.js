d2fb9e0cad053987be32e84cea1d91a5dc12592b
gonpeche <espeche.g@gmail.com>
2020-09-20 15:53:12 -0300

    understanding props in styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import Button from 'components/common/Button'
+import { Button } from 'components/common'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+`
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>App</h1>
-      <Button>Test</Button>
+      <Button>Primary</Button>
+      <Button secondary>Secondary</Button>
     </>
   )
 }
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Button = styled.button`
     color: white;
-    background: #f8049c;
+    background: ${props => props.secondary ? 'black' : '#f8049c'};
     font-weight: bold;
     padding: 8px;
     border-radius: 4px;
@@ -19,4 +19,4 @@ const Button = styled.button`
     }
 `;
 
-export default Button;
+export { Button };
