3513940d237a1a7ce10d34dd6fea34fc45ebf372
gonpeche <espeche.g@gmail.com>
2020-09-21 11:26:31 -0300

    form added

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { PageLayout } from 'components/common';
+import { PageLayout, Input } from 'components/common';
+
+import styled from 'styled-components';
+
+const Form = styled.form`
+    width: 100%;
+    max-width: 400px;
+    background: white;
+    border: 1px solid #eee;
+    padding: 16px;
+    box-sizing: border-box;
+    color: black;
+    border-radius: 4px;
+`;
 
 export default function Login() {
     return (
-        <PageLayout>Login</PageLayout>
+        <PageLayout>
+            <h1>Login</h1>
+            <Form>
+                <Input name="username" placeholder="Username" />
+            </Form>
+        </PageLayout>
     )
 }
