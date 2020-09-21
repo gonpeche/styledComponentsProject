44ee7fc66ff2cd03dfa8a26747e72c22628ea48f
gonpeche <espeche.g@gmail.com>
2020-09-20 21:38:02 -0300

    pathname feature for links

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link as ReactRouterDomLink } from 'react-router-dom';
+import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom';
 
 const HeaderWrapper = styled.header`
     height: 60px;
@@ -43,11 +43,12 @@ const StyledLink = styled(Link)`
 `;
 
 export function Header() {
+    const { pathname } = useLocation();
     return (
         <HeaderWrapper>
             <Menu>
-                <StyledLink to="/">Home</StyledLink>
-                <StyledLink isActive to="/login">Login</StyledLink>
+                <StyledLink to="/" isActive={pathname === '/'}>Home</StyledLink>
+                <StyledLink isActive={pathname === '/login'} to="/login">Login</StyledLink>
             </Menu>
         </HeaderWrapper>
     )
