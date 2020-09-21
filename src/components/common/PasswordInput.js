de5b4a05441629bb7464cbfc2d2c9f6393d60d0c
gonpeche <espeche.g@gmail.com>
2020-09-21 16:06:35 -0300

    toggle Hide Show password label

--- src/components/common/PasswordInput.js
+++ src/components/common/PasswordInput.js
@@ -10,9 +10,43 @@ const PasswordInputStyled = styled(Input).attrs(() => ({
     border-bottom-right-radius: 0;
 `;
 
+const PasswordInputWrapper = styled.div`
+    display: flex;
+    ~div {
+        margin-bottom: 8px;
+    }
+`;
+
+const ToggleButton = styled.div`
+    height: 40px;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    font-size: 0.9em;
+    display: flex;
+    padding: 8px;
+    border-left: 0;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    background: white;
+    font-weight: bold;
+    cursor: pointer;
+    user-select: none;
+    color: black;
+`;
+
 export function PasswordInput(props) {
-    const [showPassword, setShowPassord] = useState(false)
+    const [showPassword, setShowPassord] = useState(true)
     return (
-        <PasswordInputStyled {...props} />
+        <>
+            <PasswordInputWrapper>
+                <PasswordInputStyled {...props} />
+                <ToggleButton onClick={() => setShowPassord(!showPassword)}>
+                    {showPassword ? 'Hide' : 'Show'}
+                </ToggleButton>
+            </PasswordInputWrapper>
+            <div>
+                {showPassword ? props.value : '' } 
+            </div>
+        </>
     )
 }
