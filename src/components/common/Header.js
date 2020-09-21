86fb5a681e72749d43380f775f54a9942c9d1fe9
gonpeche <espeche.g@gmail.com>
2020-09-20 21:35:36 -0300

    introducing the styled(Component) feature

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link as ReactRouterDomLink } from 'react-router-dom';
 
 const HeaderWrapper = styled.header`
     height: 60px;
@@ -13,11 +13,42 @@ const HeaderWrapper = styled.header`
     top: 0;
 `;
 
+const Menu = styled.nav`
+    display: flex;
+    position: relative;
+    width: initial;
+    border-bottom: none;
+    margin: auto 0 auto auto;
+    font-family: 'Open Sans';
+    background: none;
+    left: initial;
+    top: initial;
+`;
+
+const Link = ({ isActive, children, ...props}) => {
+    return (
+        <ReactRouterDomLink {...props}>
+            {children}
+        </ReactRouterDomLink>
+    )
+}
+
+const StyledLink = styled(Link)`
+    padding: 4px 8px;
+    display: block;
+    text-align: center;
+    box-sizing: border-box;
+    margin: auto 0;
+    font-weight: ${props => props.isActive ? 'bold' : 'normal'};
+`;
+
 export function Header() {
     return (
         <HeaderWrapper>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            <Menu>
+                <StyledLink to="/">Home</StyledLink>
+                <StyledLink isActive to="/login">Login</StyledLink>
+            </Menu>
         </HeaderWrapper>
     )
 }
