6937452bc0bd592439e26fd76af4ef614f4e11a4
gonpeche <espeche.g@gmail.com>
2020-09-21 10:47:49 -0300

    header and menu responsive

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,55 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom';
+import React from 'react'
+import styled from 'styled-components'
+import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom'
 
 const HeaderWrapper = styled.header`
-    height: 60px;
-    width: 100%;
-    box-sizing: border-box;
-    display: flex;
-    padding: 0 16px;
-    position: fixed;
-    background: #eeeeee;
-    top: 0;
-`;
+  height: 60px;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  padding: 0 16px;
+  position: fixed;
+  background-image: linear-gradient(to right, #f8049c, #fdd54f);
+  border-bottom: 3px solid #fdd54f;
+  top: 0;
+`
 
 const Menu = styled.nav`
+  display: block;
+  font-family: 'Open Sans';
+  position: absolute;
+  width: 100%;
+  top: 60px;
+  left: 0;
+  padding: 8px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #fdd54f;
+  background: white;
+
+  @media (min-width: 768px) {
     display: flex;
-    position: relative;
-    width: initial;
-    border-bottom: none;
-    margin: auto 0 auto auto;
-    font-family: 'Open Sans';
     background: none;
     left: initial;
     top: initial;
-`;
+    margin: auto 0 auto auto;
+    border-bottom: none;
+    position: relative;
+    width: initial;
+  }
+`
 
-const Link = ({ isActive, children, ...props}) => {
-    return (
-        <ReactRouterDomLink {...props}>
-            {children}
-        </ReactRouterDomLink>
-    )
+const Link = ({ isActive, children, ...props }) => {
+  return <ReactRouterDomLink {...props}>{children}</ReactRouterDomLink>
 }
 
 const StyledLink = styled(Link)`
-    padding: 4px 8px;
-    display: block;
-    text-align: center;
-    box-sizing: border-box;
-    margin: auto 0;
-    font-weight: ${props => props.isActive ? 'bold' : 'normal'};
-`;
+  padding: 4px 8px;
+  display: block;
+  text-align: center;
+  box-sizing: border-box;
+  margin: auto 0;
+  font-weight: ${(props) => (props.isActive ? 'bold' : 'normal')};
+  color: black;
+`
 
 export function Header() {
-    const { pathname } = useLocation();
-    return (
-        <HeaderWrapper>
-            <Menu>
-                <StyledLink to="/" isActive={pathname === '/'}>Home</StyledLink>
-                <StyledLink isActive={pathname === '/login'} to="/login">Login</StyledLink>
-            </Menu>
-        </HeaderWrapper>
-    )
+  const { pathname } = useLocation()
+  return (
+    <HeaderWrapper>
+      <Menu>
+        <StyledLink to="/" isActive={pathname === '/'}>
+          Home
+        </StyledLink>
+        <StyledLink isActive={pathname === '/login'} to="/login">
+          Login
+        </StyledLink>
+      </Menu>
+    </HeaderWrapper>
+  )
 }
