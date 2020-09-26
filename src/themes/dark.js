a7cf46091e899487e3392043b215b15f09e7deb5
gonpeche <espeche.g@gmail.com>
2020-09-26 10:41:12 -0300

    styles to theme dark and light

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -2,6 +2,8 @@ const theme = {
     id: 'dark',
     primaryColor: 'black',
     secondaryColor: 'midnightblue',
+    bodyBackgroundColor: 'black',
+    bodyFontColor: 'white',
 }
 
   export default theme;
--- src/themes/light.js
+++ src/themes/light.js
@@ -2,6 +2,9 @@ const theme = {
     id: 'light',
     primaryColor: '#f8049c',
     secondaryColor: '#fdd54f',
+    bodyBackgroundColor: 'white',
+    bodyFontColor: 'black',
+
 }
 
   export default theme;
